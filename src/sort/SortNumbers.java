6a220c356a7c1a9fe0b4941a420198023d62a43b
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-30 10:23:42 +0100

    refactor

--- src/sort/SortNumbers.java
+++ src/sort/SortNumbers.java
@@ -53,12 +53,14 @@ public class SortNumbers {
         networkPrefixes.put("mtn", List.of("0703","0706","0803","0806", "0810", "0813", "0814", "0816", "0903", "0906","0916", "0916", "07025","07026","0704"));
         networkPrefixes.put("airtel", List.of("0701","0708","0802","0808","0812","0901","0902","0904","0907","0912"));
         networkPrefixes.put("globacom", List.of("0705","0805","0807","0811","0815","0905","0915"));
-        networkPrefixes.put("etisalat", List.of("0809","0817","0818","0909","0908"));
+        networkPrefixes.put("9mobile", List.of("0809","0817","0818","0909","0908"));
+        networkPrefixes.put("mtel", List.of("0804"));
 
         networkCount.put("mtn", 0);
         networkCount.put("airtel", 0);
         networkCount.put("globacom", 0);
-        networkCount.put("etisalat", 0);
+        networkCount.put("9mobile", 0);
+        networkCount.put("mtel", 0);
     }
 
     private static void printNetworkSummary(){
