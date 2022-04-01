efcb84ff270e6b28b18734ff128fcc9317933ac9
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-04-01 12:59:37 +0100

    refactor

---
+++ test/sort/SortNumbersTest.java
@@ -0,0 +1,17 @@
+package sort;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class SortNumbersTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+}
